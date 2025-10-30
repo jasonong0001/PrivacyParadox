0d164a1e39763347ef92136090aef89ab88c18e9
jasonong0001 <ongwlwm69352@outlook.com>
2025-10-30 16:20:38 +0800

    deploy on testnet

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedGame = await deploy("TwentyOneGame", {
+  const deployedGame = await deploy("ParadoxGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`TwentyOneGame contract: `, deployedGame.address);
+  console.log(`ParadoxGame contract: `, deployedGame.address);
 };
 export default func;
 func.id = "deploy_twenty_one_game"; // id required to prevent reexecution
-func.tags = ["TwentyOneGame"];
+func.tags = ["ParadoxGame"];
